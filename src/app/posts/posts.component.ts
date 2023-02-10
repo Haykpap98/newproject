000b6181f795926a13b006379a18834287cbbeb2
Haykpap98 <haykpap17@gmail.com>
2023-02-10 19:42:45 +0400

    posts()

---
+++ src/app/posts/posts.component.ts
@@ -0,0 +1,33 @@
+import { Component } from '@angular/core';
+import { PostsService } from '../posts.service';
+import { Posts } from '../student.model';
+
+@Component({
+  selector: 'app-posts',
+  templateUrl: './posts.component.html',
+  styleUrls: ['./posts.component.scss']
+})
+export class PostsComponent {
+  Posts!: Posts[] ;
+
+  constructor(
+    private postsService: PostsService,
+  ) {}
+
+  
+  ngOnInit(): void {
+    this.getPosts();
+
+  }
+  getPosts(){
+    this.postsService.getPoststList().subscribe((res) => {
+      this.Posts = res.map((e) => {
+        return {
+          id: e.payload.doc.id,
+          ...(e.payload.doc.data() as {}),
+        } as unknown as Posts;
+      });
+      console.log(this.Posts)
+    });
+  }
+}
